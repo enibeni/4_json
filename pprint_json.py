9e87a97d0bc9210e132bef2c147931defbe4ae85
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2017-12-23 15:19:14 +0300

    функции очищены, использована втроенная функция для чтения файла

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,16 @@ import sys
 
 
 def load_data(json_filepath):
-    print(json_filepath + "!!!")
-    json_data = ""
     with open(json_filepath, "r", encoding="utf-8") as file:
-        for line in file:
-            json_data += line
+        print(type(file))
+        json_data = file.read()
     return json_data
 
 
 def pretty_print_json(json_data):
-    print(json.dumps(json.loads(json_data, encoding="utf-8"), indent=4, ensure_ascii=False))
+    print(json.dumps(json.loads(json_data),
+                     indent=4, ensure_ascii=False))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pretty_print_json(load_data(sys.argv[1]))
